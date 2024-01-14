4ad7ac25af67dee9f29a485d961f97bae5de3f53
seizethedaylyn <rlacofls294@gmail.com>
2024-01-14 18:05:34 +0900

    20240114

---
+++ 01_14/star2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int num;
+    cin >> num;
+
+    for(int i = 0;i<num; i++){
+        for(int j = num; j > i+1 ;j--){
+            cout << " ";
+        }
+        for(int j = 0 ; j < i +1 ; j++){
+            cout << "*";
+        }
+        cout << "\n";
+    }
+}
---
+++ 01_14/sum.cpp
@@ -0,0 +1,27 @@
+/*
+문제
+n이 주어졌을 때, 1부터 n까지 합을 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 n (1 ≤ n ≤ 10,000)이 주어진다.
+
+출력
+1부터 n까지 합을 출력한다.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int num, sum;
+
+    cin >> num;
+
+    sum = 0;
+    for(int i = 1; i <= num; i++){
+        sum += i;
+    }
+
+    cout << sum;
+}
